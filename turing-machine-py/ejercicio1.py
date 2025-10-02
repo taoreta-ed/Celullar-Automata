aa2946ce5239a2765b4375a4e1361d5d620d4881
Eduardo Castro <nora.castroeduardo@gmail.com>
2025-10-02 15:39:42 -0600

    Enhance Turing machine implementation to include output file logging and update input string

--- turing-machine-py/ejercicio1.py
+++ turing-machine-py/ejercicio1.py
@@ -3,7 +3,7 @@
 #import sys
 #from collections import deque #para cintas más grandes
 
-def maquina_turing(archivo_entrada = "entrada.txt"):
+def maquina_turing(archivo_entrada = "entrada.txt", archivo_salida = "salida.txt"):
     #cinta = "0011" #list() #La cadena a leer
     #cinta = ['0', '0', '1', '1']
     
@@ -24,82 +24,110 @@ def maquina_turing(archivo_entrada = "entrada.txt"):
     #Vamos a tener diferentes condiciones dependiendo del estado y el simbolo a leer
 
     i = 0 #iterador que nos ayuda a saber en que posición de la cinta estamos
+    paso = 0 #iterador que cuenta en qué paso vamos
 
-    while estado != '4':
-        print("cinta: ", cinta)
+    try:
+        #Vamos a abrir el archivo donde guardaremos los datos
+        with open(archivo_salida, 'w') as salida:
+            salida.write("Paso | Estado | Cabezal | Cinta\n")
+
         
-        #Para el estado 0
-        if estado == '0':
-            print("Estado 0: ")
-            if cinta[i] == '0':
-                estado = '1'
-                cinta[i] = 'X'
-                i+=1
-            elif cinta[i] == 'Y':
-                estado = '3'
-                cinta[i] = 'Y'
-                i+=1
-            else:
-                print("Cadena no válida")
-                break
-        
-        #Para el estado 1
-        elif estado == '1':
-            print("Estado 1: ")
-            if cinta[i] == '0':
-                estado = '1'
-                cinta[i] = '0'
-                i+=1
-            elif cinta[i] == '1':
-                estado = '2'
-                cinta[i] = 'Y'
-                i-=1
-            elif cinta[i] == 'Y':
-                estado = '1'
-                cinta[i] == 'Y'
-                i+=1
-            else:
-                print("Cadena no válida")
-                break
+            while estado != '4':
+                print("cinta: ", cinta)
+                paso +=1
+                
+                
+                #Revisar que el iterador esté dentro de los límites
+                simbolo_leido = "" #Esta variable la usamos para comparaciones, no para asignar
+                if i < 0:
+                    #Movimiento a la izquierda, inserta un espacio en blanco ("B") y vuelve al índice 0
+                    cinta.insert(0, 'B')
+                    i = 0
+                    simbolo_leido = cinta[i]
+                elif i >= len(cinta):
+                    #Movimiento a la derecha fuera del límite, hay un Blanco
+                    #Falta agregar la lógica para manejar 'B' Si es necesario
+                    simbolo_leido = 'B'
+                else:
+                    #Todo bien (cabezal dentro de los límites)
+                    simbolo_leido = cinta[i]
 
-        #Para el estado 2
-        elif estado == '2':
-            print("Estado 2: ")
-            if cinta[i] == '0':
-                estado = '2'
-                cinta[i] = '0'
-                i-=1
-            elif cinta[i] == 'X':
-                estado = '0'
-                cinta[i] = 'X'
-                i+=1
-            elif cinta[i] == 'Y':
-                estado = '2'
-                cinta[i] = 'Y'
-                i-=1
-            else:
-                print("Cadena no válida")
-                break
+                #Log al archivo
+                salida.write(f"{paso:02d} | {estado} | {i:02d} | {"".join(cinta)}\n")
 
-        #Para el estado 3
-        elif estado == '3':
-            print("Estado 3: ")
-            if i == len(cinta):
-                estado = '4'
-                print(cinta)
-                print("Estado 4: Estado de aceptación")
-            elif cinta[i] == 'Y':
-                estado = '3'
-                cinta[i] = 'Y'
-                i+=1
-            else:
-                print("Cadena no válida")
-                break
+                #Para el estado 0
+                if estado == '0':
+                    print("Estado 0: ")
+                    if simbolo_leido == '0':
+                        estado = '1'
+                        cinta[i] = 'X'
+                        i+=1
+                    elif simbolo_leido == 'Y':
+                        estado = '3'
+                        cinta[i] = 'Y'
+                        i+=1
+                    else:
+                        print("Cadena no válida")
+                        break
+                
+                #Para el estado 1
+                elif estado == '1':
+                    print("Estado 1: ")
+                    if simbolo_leido == '0':
+                        estado = '1'
+                        cinta[i] = '0'
+                        i+=1
+                    elif simbolo_leido == '1':
+                        estado = '2'
+                        cinta[i] = 'Y'
+                        i-=1
+                    elif simbolo_leido == 'Y':
+                        estado = '1'
+                        cinta[i] == 'Y'
+                        i+=1
+                    else:
+                        print("Cadena no válida")
+                        break
 
-        #Para el estado 4
-        else:
-            print("Cadena no válida")
-            break
+                #Para el estado 2
+                elif estado == '2':
+                    print("Estado 2: ")
+                    if simbolo_leido == '0':
+                        estado = '2'
+                        cinta[i] = '0'
+                        i-=1
+                    elif simbolo_leido == 'X':
+                        estado = '0'
+                        cinta[i] = 'X'
+                        i+=1
+                    elif simbolo_leido == 'Y':
+                        estado = '2'
+                        cinta[i] = 'Y'
+                        i-=1
+                    else:
+                        print("Cadena no válida")
+                        break
 
+                #Para el estado 3
+                elif estado == '3':
+                    print("Estado 3: ")
+                    if i == len(cinta):
+                        estado = '4'
+                        print(cinta)
+                        print("Estado 4: Estado de aceptación")
+                    elif simbolo_leido == 'Y':
+                        estado = '3'
+                        cinta[i] = 'Y'
+                        i+=1
+                    else:
+                        print("Cadena no válida")
+                        break
+
+                #Para el estado 4
+                else:
+                    print("Cadena no válida")
+                    break
+    except Exception as e:
+        print("Ocurró un error al escribir o durante la simulación: " + e)
 
 maquina_turing()
